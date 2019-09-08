3e624f9a245efffef6c5b6ac4397d5e5e5470d5c
Zain Momin <55015838+MZM45@users.noreply.github.com>
2019-09-08 22:30:37 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim CARNAME As String
+        CARNAME = ""
+
+        Dim Counter As Integer
+        Counter = 0
+
+        Console.Write(" Write your car name: ")
+        CARNAME = Console.ReadLine()
+
+        For Counter = 1 To 5
+            Console.WriteLine(CARNAME)
+        Next
+
+        Console.WriteLine("Press any key to continue..")
+        Console.ReadKey()
+    End Sub
+
+End Module
